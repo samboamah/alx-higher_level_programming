eba932cae7891c96ef891184242382608d5a2d88
Samuel Boamah <skboamah16@gmail.com>
2022-12-07 02:09:08 +0000

    scripts

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -8,11 +8,3 @@ if __name__ == "__main__":
     for i in range(len(sys.argv) - 1):
         total += int(sys.argv[i + 1])
     print("{}".format(total))
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Doc
