c3a41577b05848b079e4157b0a39952d7ff2610a
Samuel Boamah <skboamah16@gmail.com>
2022-12-07 02:07:27 +0000

    scripts

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the addition of all arguments."""
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Doc
